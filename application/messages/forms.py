6ceb37f619aaef5ddefc0407e4550837168d8a18
Tommi Niittymies <tomminiittymies@Tommis-MacBook-Pro.local>
2020-01-19 20:18:30 +0200

    Added form functionality to Messages

---
+++ application/messages/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, validators
+
+class MessageForm(FlaskForm):
+    title = StringField("Otsikko", [validators.Length(min=1), validators.Length(max=50)])
+    content = TextAreaField("Viesti", [validators.Length(max=5000)])
+
+    class Meta:
+        csrf = False
--- application/messages/views.py
+++ application/messages/views.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for
 from application.messages.models import Message
+from application.messages.forms import MessageForm
 from datetime import datetime
 
 @app.route("/messages", methods=["GET"])
@@ -15,11 +16,18 @@ def message_details(message_id):
     
 @app.route("/messages/new", methods=["GET"])
 def messages_creationForm():
-    return render_template("messages/messageCreationForm.html")
+    return render_template("messages/messageCreationForm.html", form=MessageForm())
 
 @app.route("/messages", methods=["POST"])
 def messages_create():
-    newMessage = Message(request.form.get("title"), request.form.get("content"), datetime.now())
+    form = MessageForm(request.form)
+
+    if not form.validate():
+        return render_template("messages/messageCreationForm.html", form = form)
+
+    newMessage = Message(form.title.data, form.content.data, datetime.now())
+
     db.session.add(newMessage)
     db.session.commit()
+    
     return redirect(url_for("messages_list"))
