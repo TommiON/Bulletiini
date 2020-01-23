3de293de98abbe25eeb3d0bcb4a91430c9653971
Tommi Niittymies <tomminiittymies@Tommis-MacBook-Pro.local>
2020-01-24 00:09:29 +0200

    Adding PostgreSQL

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bulletins.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bulletins.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -31,5 +35,7 @@ login_manager.login_message = "Toiminto vaatii kirjautumisen."
 def load_user(user_id):
     return User.query.get(user_id)
 
-# luo tietokantataulut, mutta huom: vain tarvittaessa
-db.create_all()
+try: 
+    db.create_all()
+except:
+    pass
