114b032b9c3c46a6af69a340811a969777289ec1
Tommi Niittymies <tomminiittymies@Tommis-MacBook-Pro.local>
2020-02-05 22:52:18 +0200

    Added a frontpage to the app, made some css/bootstrap stylings

--- application/views.py
+++ application/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from application import app
+from application.threads.models import Thread
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("threadsList.html", threads=Thread.query.all())
+    # return render_template("index.html")
