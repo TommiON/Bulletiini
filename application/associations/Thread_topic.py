03d2372134b838a360d8f23ce22d7458d09ac8a2
Tommi Niittymies <tomminiittymies@Tommis-MacBook-Pro.local>
2020-02-19 14:54:57 +0200

    Heroku troubleshooting

--- application/associations/Thread_topic.py
+++ application/associations/Thread_topic.py
@@ -3,6 +3,6 @@ from application.models import Base
 
 Thread_topic = db.Table("Thread_topic", 
                         Base.metadata,
-                        db.Column("thread_id", db.Integer, db.ForeignKey("thread.id")), 
-                        db.Column("topic_id", db.Integer, db.ForeignKey("topic.id"))
+                        db.Column("thread_id", db.Integer, db.ForeignKey("thread.id"), primary_key=True), 
+                        db.Column("topic_id", db.Integer, db.ForeignKey("topic.id"), primary_key=True)
 )
