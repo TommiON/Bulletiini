ddfb54244e1b97da519aa143b3ff6966510e07b5
Tommi Niittymies <tomminiittymies@Tommis-MacBook-Pro.local>
2020-02-20 20:31:49 +0200

    Added user deletion functionality

--- application/users/views.py
+++ application/users/views.py
@@ -9,16 +9,19 @@ from datetime import datetime
 def users_list():
     return render_template("userList.html", users=User.query.all()) 
 
+
 # yksittäisen käyttäjän tiedot
 @app.route("/users/<user_id>", methods=["GET"])
 def user_details(user_id):
     user = User.query.get(user_id)
     return render_template("userDetails.html", user=user)
 
+
 # lomake uuden käyttäjän luomiseen
 @app.route("/users/new", methods=["GET"])
 def users_creationForm():
     return render_template("userCreationForm.html", form=UserCreationForm())
+
 
 # uuden käyttäjän luominen
 @app.route("/users", methods=["POST"])
@@ -33,6 +36,7 @@ def users_create():
     db.session.commit()
     return render_template("userList.html", users=User.query.all()) 
 
+
 # admin-oikeuksien asetus ja poisotto, vaatii admin-oikeudet
 @app.route("/users/<user_id>/", methods=["POST"])
 @login_required(role="ADMIN")
@@ -43,4 +47,13 @@ def user_change_admin_status(user_id):
     db.session.commit()
 
     return redirect(url_for("users_list"))
+
+# käyttäjätunnuksen poisto, vaatii admin-oikeudet
+@app.route("/users/delete/<user_id>", methods=["GET"])
+@login_required(role="ADMIN")
+def users_delete(user_id):
+    user_to_be_deleted = User.query.get(user_id)
+    db.session.delete(user_to_be_deleted)
+    db.session.commit()
+    return render_template("userList.html", users=User.query.all()) 
 
