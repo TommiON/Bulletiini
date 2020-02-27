7204913918165ae239db11d2163c84d3b0281d0e
Tommi Niittymies <tomminiittymies@Tommis-MacBook-Pro.local>
2020-02-27 20:45:40 +0200

    Database troublehooting

--- application/associations/Thread_topic.py
+++ application/associations/Thread_topic.py
@@ -2,5 +2,5 @@ from application import db
 
 Thread_topic = db.Table('Thread_topic',     
                         db.Column('thread_id', db.Integer, db.ForeignKey('thread.id', ondelete='CASCADE'), primary_key=True), 
-                        db.Column('topic_id', db.Integer, db.ForeignKey('topic.id', ondelete='CASCADE'), primary_key=True)
+                        db.Column('topic_id', db.Integer, db.ForeignKey('topic.id'), primary_key=True)
 )
